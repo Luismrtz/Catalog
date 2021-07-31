dedce77ffb9dc205521f78e9326418e6e9939f8d
luisricardo494 <luisricardo494@gmail.com>
2021-07-31 16:12:37 -0400

    Controller GET

---
+++ Catalog/Controllers/ItemsController.cs
@@ -0,0 +1,32 @@
+﻿using Catalog.Entities;
+using Catalog.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.Controllers
+{
+
+    [ApiController]
+    //[Route("[controller]")] ... used if we want to call like //GET   /times... or we can specify the specific route I want to use
+    [Route("items")]
+    //controllerbase turns into a controller class
+    public class ItemsController : ControllerBase
+    {
+        private readonly InMemItemsRepository repository;
+
+        public ItemsController()
+        {
+            repository = new InMemItemsRepository();
+        }
+        // Get / items
+        [HttpGet]
+        public IEnumerable<Item> GetItems()
+        {
+            var items = repository.GetItems();
+            return items;
+        }
+    }
+}
---
+++ Catalog/Entities/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.Entities
+{
+    public record Item
+    {
+        //our goal is that we need this immutable, so either "init;" or...
+        //"private set;" while this would make it immutable, it is trickier to implement,
+        //because now we will need to implement a constructor, and navigating our objects would be clunkier for our purpose
+        public Guid Id { get; init; } // so Item item = new() { id = Guid.NewGuid() } .... works because "NEW()" makes immutable, WHILE "item.Id = Guid.NewGuid();" DOES NOT work
+        public string Name { get; init; }
+        public decimal Price { get; init; }
+        public DateTimeOffset CreatedDate { get; init; }
+    }
+}
